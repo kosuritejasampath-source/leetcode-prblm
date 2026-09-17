cd5e6383905d421087c90fc25d73e92e3de69ef5
kosuritejasampath-source <kosuritejasampath@gmail.com>
2026-09-17 22:22:15 +0530

    Time: 11 ms (95.94%) | Memory: 12.5 MB (86.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,23 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if(sorted(s)==sorted(t)):
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        freq1={}
+        freq2={}
+        for ch in s:
+            if ch in freq1:
+                freq1[ch]+=1
+            else:
+                freq1[ch]=1
+        for ch in t:
+            if ch in freq2:
+                freq2[ch]+=1
+            else:
+                freq2[ch]=1
+        if freq1==freq2:
             return True
         else:
             return False
